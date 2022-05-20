df4be56e92eedf2744619eba4051809971d79208
meanseo <chlalstjok97@gmail.com>
2022-05-20 17:27:20 +0900

    22.05.20 17:26 검색창 및 버튼 추가

--- components/Search.js
+++ components/Search.js
@@ -1,14 +1,14 @@
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+import styles from '../styles/Search.module.css'
 
 export function Search(){
     return(
         <>
-        <input autoComplete="검색어를 입력하세요"></input>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
+        <div className= {styles.center}>
+        <input></input> <button>텍스트 검색</button> <button>사진 검색</button>
+        </div>
+        <div>
+        <image scr="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/picture-512.png"/>
+        </div>
         </>
-
     )
 }
