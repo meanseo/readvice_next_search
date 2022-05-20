4807c996ae40aae0dc0e12960445794e07ab1cf8
meanseo <chlalstjok97@gmail.com>
2022-05-20 23:05:52 +0900

    23:05 사진 추가

--- components/Search.js
+++ components/Search.js
@@ -6,9 +6,9 @@ export function Search(){
         <div className= {styles.center}>
         <input></input> <button>텍스트 검색</button> <button>사진 검색</button>
         </div>
-        <div>
-        <image scr="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/picture-512.png"/>
-        </div>
+        <p><img src="http://image.kyobobook.co.kr/images/book/large/225/l9791191114225.jpg" align="middle"/>작별인사</p>
+        <p><img src="http://image.kyobobook.co.kr/images/book/large/610/l9788972773610.jpg" align="middle"/>가</p>
+        <p><img src="http://image.kyobobook.co.kr/images/book/large/665/l9791166686665.jpg" align="middle"/>나</p>
         </>
     )
 }
